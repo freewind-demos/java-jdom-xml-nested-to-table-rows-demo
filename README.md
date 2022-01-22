6f77651384bf914b9d0c11fc746f4ceb57e68e9a
Freewind <nowindlee@gmail.com>
2022-01-23 03:36:24 +0800

    java-jdom-parse-xml-demo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Java Hello World Demo
+Java JDom Parse XML Demo
 =====================
 
 Use maven for Java.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,11 +1,34 @@
 package demo;
 
-import org.apache.commons.lang3.StringUtils;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.InputStream;
+import java.util.List;
 
 public class Hello {
 
-  public static void main(String[] args) {
-    System.out.println(StringUtils.trim("Hello World!  "));
+  public static void main(String[] args) throws Exception {
+
+    try (InputStream input = Hello.class.getClassLoader().getResourceAsStream("simple.xml")) {
+      SAXBuilder saxBuilder = new SAXBuilder();
+      Document document = saxBuilder.build(input);
+
+      Element root = document.getRootElement();
+      System.out.println("### root: " + root.getName());
+
+      List<Element> students = root.getChildren();
+      System.out.println("----------------------------");
+
+      for (Element student : students) {
+        System.out.println("### student :" + student.getName());
+        System.out.println("### id : " + student.getAttribute("id").getValue());
+        System.out.println("### First Name : " + student.getChild("firstname").getText());
+      }
+    }
+
   }
 
 }
